ad3442969fbf60bfbaa13ac6cd131cff102577d0
Katie Poeltler <kpoeltler@gmail.com>
2018-09-14 17:07:21 -0500

    Set background position and scale

---
+++ web/js/main-menu.js
@@ -0,0 +1,24 @@
+class MainMenu extends Phaser.Scene{}
+
+//Load assets
+MainMenu.preload = function() {
+	//load background image
+	this.load.image("background", "./assets/space.png");
+
+};
+
+MainMenu.create = function() {
+	// create background sprite
+	let spaceBackground = this.add.sprite (0, 0, "background");
+
+	//change the origin to the top left corner, use the method setOrgin(0,0)
+	spaceBackground.setOrigin(0, 0);
+
+	//change the background scale
+	spaceBackground.setScale(1);
+
+	//center the background sprite
+	spaceBackground.setPosition(640/2, 320/2);
+
+	console.log('spaceBackground testing center position', spaceBackground);
+};
