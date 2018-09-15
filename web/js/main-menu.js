783f0acd857453b341948eb57f6589bbd4550237
Jeremy Brimeyer <jeremy@staticdouble.com>
2018-09-14 19:34:52 -0500

    Add game title image

--- web/js/main-menu.js
+++ web/js/main-menu.js
@@ -5,13 +5,18 @@ class MainMenu extends Phaser.Scene {
 
 	preload() {
 		this.load.image('background', 'assets/background-space.png');
+		this.load.image('title', 'assets/text-gravity-cannon.png');
 	}
 
 	create() {
 		this.bgImage = this.add.image(0, 0, 'background');
 		this.bgImage.setOrigin(0, 0);
-
 		// TODO: Maintain background image aspect ratio
 		this.bgImage.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height);
+
+
+		this.titleImage = this.add.image(this.sys.canvas.width / 2, 100, 'title');
+		// TODO: Set scale based on canvas size
+		this.titleImage.setScale(0.4);
 	}
 }
