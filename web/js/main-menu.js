0b218d8587b727e712d4fb6073f355cb7ce39f37
Jeremy Brimeyer <jeremy@staticdouble.com>
2018-09-16 20:17:43 -0500

    Use a grid for layout of worlds on the main menu

--- web/js/main-menu.js
+++ web/js/main-menu.js
@@ -6,50 +6,106 @@ class MainMenu extends Phaser.Scene {
 	preload() {
 		this.load.image('background-space', 'assets/background-space.jpg');
 		this.load.image('text-gravity-cannon', 'assets/text-gravity-cannon.png');
-		this.load.image('world-earth', 'assets/world-earth.png');
-		this.load.image('world-io', 'assets/world-io.png');
-		this.load.image('world-mars', 'assets/world-mars.png');
-		this.load.image('world-moon', 'assets/world-moon.png');
-		this.load.image('world-pluto', 'assets/world-pluto.png');
-		this.load.image('world-venus', 'assets/world-venus.png');
+
+		worlds.forEach((world) => {
+			this.load.image(world.id, world.image);
+		});
 	}
 
 	create() {
-		this.bgImage = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'background-space');
-		this.bgAspectRatio = this.bgImage.width / this.bgImage.height;
-		this.gameAspectRatio = this.sys.canvas.width / this.sys.canvas.height;
-		this.gameScale = this.gameAspectRatio > this.bgAspectRatio ?
-			this.sys.canvas.width / this.bgImage.width :
-			this.sys.canvas.height / this.bgImage.height
+		const canvas = {
+			width: this.sys.canvas.width,
+			height: this.sys.canvas.height,
+			centerX: this.sys.canvas.width / 2,
+			centerY: this.sys.canvas.height / 2
+		};
 
-		this.bgImage.setScale(this.gameScale);
+		this.placeBackgroundImage(canvas);
+		const titleHeight = this.placeTitleImage(canvas, {top: 30, bottom: 40, sides: 200});
+		const gridCells = this.buildGrid(canvas, titleHeight, worlds.length, 3);
 
-		this.titleImage = this.add.image(this.sys.canvas.width / 2, 150 * this.gameScale, 'text-gravity-cannon');
-		this.titleImage.setScale(this.gameScale);
-
-		this.worlds = {};
-		this.addWorld('earth', this.sys.canvas.width / 2 - 900 * this.gameScale, 600 * this.gameScale);
-		this.addWorld('moon', this.sys.canvas.width / 2, 600 * this.gameScale);
-		this.addWorld('mars', this.sys.canvas.width / 2 + 900 * this.gameScale, 600 * this.gameScale);
-		this.addWorld('pluto', this.sys.canvas.width / 2 - 900 * this.gameScale, 1200 * this.gameScale);
-		this.addWorld('io', this.sys.canvas.width / 2, 1200 * this.gameScale);
-		this.addWorld('venus', this.sys.canvas.width / 2 + 900 * this.gameScale, 1200 * this.gameScale);
-
-		window.mainMenu = this;
+		for (let i = 0; i < gridCells.length; i++) {
+			this.addWorld(worlds[i], gridCells[i]);
+		}
 	}
 
-	addWorld(worldName, x, y) {
-		const world = this.add.image(x, y, `world-${worldName}`)
-			.setScale(this.gameScale)
-			.setInteractive({useHandCursor: true})
-			.on('pointerover', () => {
-				world.setScale(this.gameScale + 0.02);
-			}).on('pointerout', () => {
-				world.setScale(this.gameScale);
-			}).on('pointerup', () => {
-				alert(`You clicked ${worldName}!`);
+	placeBackgroundImage(canvas) {
+		this.bgImage = this.add.image(canvas.centerX, canvas.centerY, 'background-space');
+		this.bgImage.setScale(
+			Math.max(
+				canvas.width / this.bgImage.width,
+				canvas.height / this.bgImage.height
+			)
+		);
+	}
+
+	placeTitleImage(canvas, padding) {
+		this.titleImage = this.add.image(0, 0, 'text-gravity-cannon');
+		const maxHeight = canvas.height * .08;
+		let scale = 1;
+		let height = this.titleImage.height;
+		if (this.titleImage.height > maxHeight) {
+			scale = maxHeight / this.titleImage.height;
+			height = height * scale;
+		}
+
+		this.titleImage
+			.setPosition(canvas.centerX, height / 2 + padding.top)
+			.setScale(scale);
+
+		return height + padding.top + padding.bottom;
+	}
+
+	buildGrid(canvas, startY, slots, perRow) {
+		const numRows = Math.ceil(slots / perRow);
+		const gridHeight = canvas.height - startY;
+		const cellHeight = gridHeight / numRows;
+		const cellWidth = canvas.width / perRow;
+		const cells = [];
+
+		let x = 0;
+		let y = startY;
+		for (let i = 1; i <= slots; i++) {
+			cells.push({
+				x, y,
+				width: cellWidth,
+				height: cellHeight,
+				centerX: x + (cellWidth / 2),
+				centerY: y + (cellHeight / 2),
 			});
 
-		this.worlds[worldName] = world;
+			x += cellWidth;
+
+			if (i % perRow === 0) {
+				x = 0;
+				y += cellHeight;
+			}
+		}
+
+		return cells;
+	}
+
+	addWorld(world, cell) {
+		const image = this.add.image(cell.centerX, cell.centerY, world.id);
+		let scale = 1;
+		let padding = 40;
+
+		if (image.width > cell.width || image.height > cell.height) {
+			scale = Math.min(
+				(cell.width - (padding * 2)) / image.width,
+				(cell.height - (padding * 2)) / image.height
+			);
+		}
+
+		image
+			.setScale(scale)
+			.setInteractive({useHandCursor: true})
+			.on('pointerover', () => {
+				image.setScale(scale + 0.02);
+			}).on('pointerout', () => {
+				image.setScale(scale);
+			}).on('pointerup', () => {
+				alert(`You clicked ${world.name}!`);
+			});
 	}
 }
